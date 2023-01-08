01aa7341c88665f027020d1f4a400bdabb51067b
Meagan <meamckee89@gmail.com>
2023-01-08 13:22:55 -0500

    playRound function complete

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-console.log("Hello World!");
+const getComputerChoice = () => {
+  const computerOptions = ["rock", "paper", "scissors"];
+  const computerSelection = computerOptions[Math.floor(Math.random() * 3)];
+  return computerSelection;
+};
+
+const playRound = (playerSelection, computerSelection) => {
+  if (playerSelection === computerSelection) {
+    return "It's a draw!";
+  } else if (
+    computerSelection === "rock" &&
+    playerSelection.toLowerCase() === "paper"
+  ) {
+    return "You win! Paper beats Rock!";
+  } else if (
+    computerSelection === "rock" &&
+    playerSelection.toLowerCase() === "scissors"
+  ) {
+    return "You lose! Rock beats Scissors!";
+  } else if (
+    computerSelection === "paper" &&
+    playerSelection.toLowerCase() === "rock"
+  ) {
+    return "You lose! Paper beats Rock!";
+  } else if (
+    computerSelection === "paper" &&
+    playerSelection.toLowerCase() === "scissors"
+  ) {
+    return "You win! Scissors beats Paper!";
+  } else if (
+    computerSelection === "scissors" &&
+    playerSelection.toLowerCase() === "paper"
+  ) {
+    return "You lose! Scissors beats Paper!";
+  } else if (
+    computerSelection === "scissors" &&
+    playerSelection.toLowerCase() === "rock"
+  ) {
+    return "You win! Rock beats Scissors!";
+  }
+};
+
+const playerSelection = "ROck";
+const computerSelection = getComputerChoice();
+
+const game = () => playRound(playerSelection, computerSelection);
+console.log(computerSelection);
+console.log(game());
